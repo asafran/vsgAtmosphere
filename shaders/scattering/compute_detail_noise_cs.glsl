f786fc01a06377e018c5fe8af0c7af4de9583b50
git <asafr@yandex.ru>
2023-11-06 22:20:31 +0300

    Add clouds shaders

---
+++ shaders/scattering/compute_detail_noise_cs.glsl
@@ -0,0 +1,36 @@
+#include "noise.glsl"
+
+// ------------------------------------------------------------------
+// INPUTS -----------------------------------------------------------
+// ------------------------------------------------------------------
+
+layout(local_size_x_id = 0, local_size_y_id = 0, local_size_z_id = 0) in;
+
+// ------------------------------------------------------------------
+// UNIFORMS ---------------------------------------------------------
+// ------------------------------------------------------------------
+
+layout(binding = 0, rgba16f) uniform image3D detail_noise;
+
+layout (constant_id = 41) const int size = 32;
+
+// ------------------------------------------------------------------
+// MAIN -------------------------------------------------------------
+// ------------------------------------------------------------------
+
+void main()
+{
+    vec3 tex_coord = (vec3(gl_GlobalInvocationID) + vec3(0.5f)) / float(size);
+
+    float freq = 8.0f;
+
+    float worley0 = worley_fbm(tex_coord, freq);
+    float worley1 = worley_fbm(tex_coord, freq * 2.0f);
+    float worley2 = worley_fbm(tex_coord, freq * 4.0f);
+ 
+    vec4 worley = vec4(worley0, worley1, worley2, 0.0f); 
+    
+    imageStore(detail_noise, ivec3(gl_GlobalInvocationID), worley);
+}
+
+// ------------------------------------------------------------------
---
+++ shaders/scattering/compute_shape_noise_cs.glsl
@@ -0,0 +1,41 @@
+#include "noise.glsl"
+
+// ------------------------------------------------------------------
+// INPUTS -----------------------------------------------------------
+// ------------------------------------------------------------------
+
+layout(local_size_x_id = 0, local_size_y_id = 0, local_size_z_id = 0) in;
+
+// ------------------------------------------------------------------
+// UNIFORMS ---------------------------------------------------------
+// ------------------------------------------------------------------
+
+layout(binding = 0, rgba16f) uniform image3D shape_noise;
+
+layout (constant_id = 42) const int size = 32;
+
+// ------------------------------------------------------------------
+// MAIN -------------------------------------------------------------
+// ------------------------------------------------------------------
+
+void main()
+{
+    vec3 tex_coord = (vec3(gl_GlobalInvocationID) + vec3(0.5f)) / float(size);
+
+    float perlin = mix(1.0f, perlin_fbm(tex_coord, 4.0f, 7), 0.5f);
+    perlin = abs(perlin * 2. - 1.); // billowy perlin noise
+    
+    float freq = 4.0f;
+
+    float worley0 = worley_fbm(tex_coord, freq);
+    float worley1 = worley_fbm(tex_coord, freq * 2.0f);
+    float worley2 = worley_fbm(tex_coord, freq * 4.0f);
+ 
+    float perlin_worley = remap(perlin, 0.0f, 1.0f, worley0, 1.0f); // perlin-worley
+ 
+    vec4 cloud = vec4(perlin_worley, worley0, worley1, worley2);
+
+    imageStore(shape_noise, ivec3(gl_GlobalInvocationID), cloud);
+}
+
+// ------------------------------------------------------------------
